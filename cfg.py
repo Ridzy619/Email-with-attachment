1a029fd3f488362a3a77a990c9c833e38b4fcb5a
Ridwan Salahuddeen <ridzy619@yahoo.com>
2020-06-25 16:31:16 +0100

    First commit: cfg.py contains credentials for connecting to the exchange server. exchangelib_email.py sends an email with attachment

---
+++ cfg.py
@@ -0,0 +1,4 @@
+creds = {"user": "salahuddeenr@accessbankplc.com",
+         "password":"xxxxxxxxxxxxxxx",
+         'server':'outlook.office365.com',
+         'smtp_address':'salahuddeenr@accessbankplc.com'}
---
+++ exchangelib_email.py
@@ -0,0 +1,57 @@
+from exchangelib import Credentials, Configuration, Account, DELEGATE
+from exchangelib import Message, Mailbox, FileAttachment
+
+from cfg import creds as cfg  # load your credentials
+
+
+def send_email(account, subject, body, recipients, attachments=None):
+    """
+    Send an email.
+
+    Parameters
+    ----------
+    account : Account object
+    subject : str
+    body : str
+    recipients : list of str
+        Each str is and email adress
+    attachments : list of tuples or None
+        (filename, binary contents)
+
+    Examples
+    --------
+    >>> send_email(account, 'Subject line', 'Hello!', ['info@example.com'])
+    """
+    to_recipients = []
+    for recipient in recipients:
+        to_recipients.append(Mailbox(email_address=recipient))
+    # Create message
+    m = Message(account=account,
+                folder=account.sent,
+                subject=subject,
+                body=body,
+                to_recipients=to_recipients)
+
+    # attach files
+    for attachment_name, attachment_content in attachments or []:
+        file = FileAttachment(name=attachment_name, content=attachment_content)
+        m.attach(file)
+    m.send_and_save()
+
+
+credentials = Credentials(username=cfg['user'],
+                             password=cfg['password'])
+
+config = Configuration(server=cfg['server'], credentials=credentials)
+account = Account(primary_smtp_address=cfg['smtp_address'], config=config,
+                  autodiscover=False, access_type=DELEGATE)
+
+# Read attachment
+attachments = []
+with open('exchangelib_email.py', 'rb') as f:
+    content = f.read()
+attachments.append(('exchangelib_email.py', content))
+
+# Send email
+send_email(account, 'Sent With the Automation', 'Just to be sure it works', ['ridzy619@gmail.com', 'henry4ally2k@gmail.com'],
+           attachments=attachments)
